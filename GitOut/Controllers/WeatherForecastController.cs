c88345e62588ae6af3f96e6f8c3fb430949e16d4
N0thaha <133283430+N0thaha@users.noreply.github.com>
2026-07-16 19:38:09 -0400

    added code

--- GitOut/Controllers/WeatherForecastController.cs
+++ GitOut/Controllers/WeatherForecastController.cs
@@ -1,33 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace GitOut.Controllers
+namespace GitOut.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [HttpGet]
+    public IActionResult GetName()
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        // Replace with your real name
+        return Ok(new { name = "Ethan Johnson" });
     }
 }
